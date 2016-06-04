00a773b8346820d176508c73efc1d204d4f71362
Dan Zabinski <dzabinski@peerless-av.com>
2016-06-04 15:20:14 -0500

    Update doc (#7)
    * Original DZ SQL Code Moved to Git
    
    From local machine I have created a get repository for all of my current
    SQL statements.  This will help me better track all of my code changes
    and keep versions and insync.
    
    * GitCommandDocument
    
    DZ -- Working on overall text document for git commands based on
    scenario
    
    * Updateingdocumentation for using git inse
    
    * done
    
    * NewFile when doing IFS development, will map a drive

---
+++ windowsCommands/MapNetworkDriveForIFSDevelopment.bat
@@ -0,0 +1,19 @@
+rem *************************************************************
+rem *															*
+rem *   Author: Dan Zabinski									*
+rem *  Created: 6/1/2016										*
+rem * Modified: 6/1/2016										*
+rem *															*
+rem *************************************************************
+rem *  Purpose:													*
+rem * Use this comment to map the Z Drive on DZ tablet 	        *
+rem * when using report writer / report designer for IFS        *
+rem * 															*
+rem *************************************************************
+rem *                                                           *
+rem * Change Log												*
+rem *  Author:			Date Changed:    	Description:		*
+rem *  DAZ				6/1/2016			Initial Creation	*
+rem * 															*
+rem *************************************************************
+net use Z: \\dc0211us\mis\IFS75\Delivery\F1ToolDisk_External
