73e0a820683f7b0f419dd435a3e54ffc62b08d59
Dan Zabinski <dzabinski@peerless-av.com>
2017-01-04 16:28:47 -0600

    Updated standard.

--- SQL/_Labor_Reports_master_source2016.sql
+++ SQL/_Labor_Reports_master_source2016.sql
@@ -100,17 +100,17 @@
                                   AND g.bom_type_db <> 'F')
                         WHEN 'Hours/Unit'
                         THEN
-                           (CASE a.run_standard * standard_crewsize
+                           (CASE a.run_standard
                                WHEN 0 THEN 0
                                ELSE ROUND (a.r_prod_qty * a.run_standard, 2)
                             END)
                         WHEN 'Hours'
                         THEN
-                           a.run_standard * standard_crewsize
+                           a.run_standard
                         ELSE
                            (CASE a.run_standard
                                WHEN 0 THEN 0
-                               ELSE ROUND ((a.r_prod_qty / a.run_standard) * standard_crewsize, 2)
+                               ELSE ROUND ((a.r_prod_qty / a.run_standard), 2)
                             END)
                      END)
                        Stnd_Production_Work_Hours,
