fe4c841293e463266914355069f17a8c1709fab9
Dan Zabinski <dzabinski@peerless-av.com>
2018-09-26 14:59:50 -0500

    Created LY script to update agile addresses

---
+++ SQL/Agile Scripts/UpdateReturnAddressLY2018.sql
@@ -0,0 +1,9 @@
+insert into _ReturnToAddresses (R2Identifier, R2Company, R2Contact, R2Address1, R2Address2, R2City, R2State, R2Zip, R2Country, R2Phone)
+VALUES ('FRED MEYER DIRECT','Fred Meyer Direct - RCX','Reclamation Center','11502 SE Hwy 212',
+'','Clackamas','OR','97015','US','630-375-5100');
+
+insert into _ReturnToAddresses (R2Identifier, R2Company, R2Contact, R2Address1, R2Address2, R2City, R2State, R2Zip, R2Country, R2Phone)
+VALUES ('W W GRAINGER','W W GRAINGER','Shipping Department','2300 White Oak Circle',
+'','Aurora','IL','60502','US','630-375-5100');
+
+--Created for Laura Young, TI 60592
