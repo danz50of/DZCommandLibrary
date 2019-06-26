19127efcf3ba4d662550c3bef05c1490a70d6e70
Dan Zabinski <dzabinski@peerless-av.com>
2019-06-26 15:57:48 -0500

    Updated mountfinder headers for csv

--- SQL/Website/2019 Website/BuildMountfinderInput.sql
+++ SQL/Website/2019 Website/BuildMountfinderInput.sql
@@ -1,3 +1,3 @@
-select 'Type','Value' from d6_prod_ci.displays where 1 = 0
+select 'type','value' from d6_prod_ci.displays where 1 = 0
 union
 SELECT 1, model FROM d6_prod_ci.displays where id >= '37484';
