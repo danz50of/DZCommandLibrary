c3a084a88144131d9b80ba23fb19194b8bbcd3f7
Dan Zabinski <dzabinski@peerless-av.com>
2020-02-02 09:34:18 -0600

    2018 table code created.  Plan to use for future development     DZ 2-2-20

---
+++ SQL/PeerlessAV Analytics Dashboard/CreateBaseAnalyticTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE EOM_2018 (
+    AccountNumber varchar(10),
+    AccountName varchar(255),
+    City varchar(255),
+    State varchar(2),
+    Zip varchar(5),
+    NationalDirector varchar(10),
+    Rep varchar(10),
+    Year Date(6),
+    Year_Char varchar(4),
+    Year_Gross float,
+    Year_Net float
+);
+
+--DZ  2-2-2020:  Create 2018 EOM table to begin analytics for 2018 v 2019
