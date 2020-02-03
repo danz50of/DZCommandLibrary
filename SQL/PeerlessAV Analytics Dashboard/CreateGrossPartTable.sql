11e3db0421f8416bfc511f63f9122651dbdf2fa6
Dan Zabinski <dzabinski@peerless-av.com>
2020-02-02 22:45:50 -0600

    New Part Tables for PowerBI

---
+++ SQL/PeerlessAV Analytics Dashboard/CreateGrossPartTable.sql
@@ -0,0 +1,17 @@
+CREATE TABLE EOM_2019 (
+    Part varchar(255),
+    Sales_Group varchar(15),
+    Sales_Group_Desc varchar(255),
+    AccountNumber varchar(20),
+    AccountName varchar(255),
+    Inv_Date Date,
+    Rep varchar(20),
+    State varchar(20),
+    Sale_AMT float,
+    USD_Amount float,
+    SITE varchar(5),
+    Year varchar(4)
+);
+
+--DZ  2-2-2020:  Create 2018 EOM table to begin analytics for 2018 v 2019
+--               When creating new tables, use script, change year!!
